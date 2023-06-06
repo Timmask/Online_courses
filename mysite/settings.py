a253fd9294faa9bb70077bcdf6b7fd5af3bd9f7b
Timmask <timakhamza@gmail.com>
2023-06-06 22:10:45 +0600

    UPDATED

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c4zf6gghj4409o(0mw&ohm#b4xly$9tfk&f$ijpz1lvq^^--5#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
